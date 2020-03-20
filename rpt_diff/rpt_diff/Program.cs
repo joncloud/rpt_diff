e4ce7f73add1d44fa673e7e3d2329e656672dbbe
joncloud <jonathan.berube@sprbrk.com>
2020-03-20 16:18:04 -0700

    Fixes usage, and make model number optional

--- rpt_diff/rpt_diff/Program.cs
+++ rpt_diff/rpt_diff/Program.cs
@@ -31,6 +31,7 @@ namespace rpt_diff
             int ModelNumber = 0;
             switch (args.Length)
             {
+                case 2:
                 case 3:
                     {
                         if (!File.Exists(args[0]))
@@ -39,7 +40,11 @@ namespace rpt_diff
                             WriteUsage();
                             return (int)ExitCode.WrongRptFile;
                         }
-                        if (!Int32.TryParse(args[2], out ModelNumber) || (ModelNumber != 0 && ModelNumber != 1))
+                        if (args.Length == 2)
+                        {
+                            ModelNumber = 1;
+                        }
+                        else if (!Int32.TryParse(args[2], out ModelNumber) || (ModelNumber != 0 && ModelNumber != 1))
                         {
                             Console.Error.WriteLine("Error: Wrong ModelNumber select - must be 0 or 1");
                             WriteUsage();
@@ -70,10 +75,9 @@ namespace rpt_diff
         }
         static void WriteUsage()
         {
-            Console.WriteLine("Usage: rpt_diff.exe DiffUtilPath RPTPath1 [RPTPath2] ModelNumber");
-            Console.WriteLine("       DiffUtilPath - Full path to external diff application .exe file that can compare two xml files (for example KDiff)");
-            Console.WriteLine("       RPTPath1 - Full path to first .rpt file to be converted to xml");
-            Console.WriteLine("       RPTPath2 - Full path to second .rpt file to be converted to xml and compared with first file");
+            Console.WriteLine("Usage: rpt_diff.exe RPTPath XMLPath [ModelNumber]");
+            Console.WriteLine("       RPTPath - Full path to .rpt file to be converted to xml");
+            Console.WriteLine("       XMLPath - Full path to where the xml file is written to");
             Console.WriteLine("       ModelNumber - Select between object model 0 - ReportDocument or 1 (recomended)- ReportClientDocument ");
         }
     }
